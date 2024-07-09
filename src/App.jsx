feec3ce2649ebdafd3bfafed95c1f2257a4d3f62
AdarshGithub18 <chouguleadarsh123@gmail.com>
2024-07-09 12:29:02 +0530

    Added react router dom and created some navigation to other pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,31 @@ import Header from './components/Header';
 import Cards from './components/Cards';
 import movies from './data/Movies.json';
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Explore from './pages/Explore';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  const cardData = movies.map((movie) => {
-    return <Cards movieID={movie.imdbID} item={movie} />;
+  const cardData = movies.map((movie, index) => {
+    return <Cards key={index} item={movie} />;
   });
 
   return (
     <>
-      <Header />
-      <section className="movie-list">{cardData}</section>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path="/Home"
+            element={<section className="movie-list">{cardData}</section>}
+          />
+          <Route path="/About" element={<About />} />
+          <Route path="/Explore" element={<Explore />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 };
